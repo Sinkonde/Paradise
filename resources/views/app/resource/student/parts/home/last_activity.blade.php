a21def1590d1f7046edc35c117021b947f9088be
Sinkonde <raphaelsinkonde1991@gmail.com>
2021-07-03 21:48:31 +0300

    last exam

--- resources/views/app/resource/student/parts/home/last_activity.blade.php
+++ resources/views/app/resource/student/parts/home/last_activity.blade.php
@@ -6,6 +6,6 @@
     @else
     <p class="md:text-xl md:text-gray-400 md:font-thin md:mt-3 mt-2  italic md:not-italic">No any activity so far</p>
     @endif
-    <a href="{{route('students.show',['student'=>$student->id, 'link'=>'t'])}}" class="text-xs mt-2 md:mt-3 md:text-xs text-blue-400 md:text-blue-300 md:hover:text-blue-500 font-thin">View all activities >></a>
+    <a href="{{route('students.show',['student'=>$student->id, 'link'=>'t'])}}" class="text-xs mt-2 md:mt-3 md:text-xs text-blue-500 md:text-blue-300 md:hover:text-blue-500 font-semibold md:font-thin">View all activities >></a>
 </div>
 @endsection
--- resources/views/app/resource/student/parts/home/last_results.blade.php
+++ resources/views/app/resource/student/parts/home/last_results.blade.php
@@ -1,12 +1,12 @@
 @section('last-results')
 <div class="w-full rounded-lg py-4 px-4 flex flex-col shadow-lg border bg-white mb-5">
-    <p class="text-xs font-semibold text-black">Latest Results</p>
+    <p class="text-xs font-semibold text-gray-400 md:text-black">Latest Results</p>
     @inject('exams', 'App\Models\Exam')
     @inject('subject', 'App\Models\SubjectTeacher')
     @foreach (array_slice($results, 0,1) as $exam =>$result)
         @if ($exams->find($exam))
-            <p class="text-xl text-gray-400 mt-3 font-thin">{{title($exams->find($exam)->name)}}</p>
-            <table class="text-xs text-gray-700 font-thin w-full">
+            <p class="md:text-xl md:font-thin  mt-2 mb-2 md:mb-0 md:mt-3 text-gray-700 md:text-gray-400">{{title($exams->find($exam)->name)}}</p>
+            <table class="hidden md:table text-xs text-gray-700 font-thin w-full">
                 <tr>
                     <th class="border text-center">Subject</th>
                     @foreach ($result['subjects'] as $s => $mark)
@@ -33,7 +33,28 @@
                     <td class="border text-center font-semibold" title="Position">{{$result['position']}}</td>
                 </tr>
              </table>
-             <a href="{{route('students.show',['student'=>$student->id, 'link'=>'a'])}}" class="mt-3 text-xs text-blue-300 hover:text-blue-500 font-thin">View all results >></a>
+             <div class="md:hidden flex flex-row">
+                @foreach ($result['subjects'] as $s => $mark)
+                        @if ($subject->find($subject))
+                            <span class="mr-1 text-xs text-black bg-yellow-400 rounded-full px-1 py-1">
+                                {{strtolower($subject->find($s)->class_subject->level_subject->subject->short)}}-{{$mark}}
+                            </span>
+                        @endif
+                    @endforeach
+
+                    <span class="mr-1 text-xs text-black bg-yellow-400 rounded-full px-1 py-1">
+                        avg-{{round(array_sum($result['subjects'])/count($result['subjects']),2)}}
+                    </span>
+
+                    <span class="mr-1 text-xs text-black bg-yellow-400 rounded-full px-1 py-1">
+                        grd-{{setGrade(array_sum($result['subjects'])/count($result['subjects']))}}
+                    </span>
+
+                    <span class="mr-1 text-xs text-black bg-yellow-400 rounded-full px-1 py-1">
+                        post-{{$result['position']}}
+                    </span>
+             </div>
+             <a href="{{route('students.show',['student'=>$student->id, 'link'=>'a'])}}" class="text-xs mt-2 md:mt-3 md:text-xs text-blue-500 md:text-blue-300 md:hover:text-blue-500 font-semibold md:font-thin">View all results >></a>
         @endif
     @endforeach
 
