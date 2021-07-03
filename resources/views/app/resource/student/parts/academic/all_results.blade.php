47c47e859fd72c9505746e0a4bc4d57a88d5464b
Sinkonde <raphaelsinkonde1991@gmail.com>
2021-07-03 22:20:00 +0300

    try out

--- resources/views/app/resource/student/parts/academic/all_results.blade.php
+++ resources/views/app/resource/student/parts/academic/all_results.blade.php
@@ -6,13 +6,13 @@
     @foreach ($results as $exam =>$result)
         @if ($exams->find($exam))
             <div class="flex justify-between pr-4">
-                <p class="text-xl text-gray-400 mt-3 font-thin">{{$loop->iteration}}. {{title($exams->find($exam)->name)}}</p>
+                <p class="md:text-xl md:font-thin  mt-2 mb-2 md:mb-0 md:mt-3 text-gray-700 md:text-gray-400">{{$loop->iteration}}. {{title($exams->find($exam)->name)}}</p>
                 @if (is_connected())
-                <a href="{{route('mail.send_results', ['student'=>$student->id, 'exam'=>$exam])}}" class="px-2 py-1 mb-1 rounded-full text-xs text-teal-500 hover:bg-teal-50 mt-3 font-thin">Send to Parent</a>
+                <a href="{{route('mail.send_results', ['student'=>$student->id, 'exam'=>$exam])}}" class="px-2 py-1 mb-1 rounded-full text-xs text-teal-500 hover:bg-teal-50 mt-3 md:font-thin">Send to Parent</a>
                 @endif
             </div>
             <div class="px-4">
-                <table class="text-xs text-gray-700 font-thin w-full">
+                <table class="hidden md:table text-xs text-gray-700 font-thin w-full">
                     <tr>
                         <th class="border text-center">Subject</th>
                         @foreach ($result['subjects'] as $s => $mark)
@@ -39,6 +39,34 @@
                         <td class="border text-center font-semibold" title="Position">{{$result['position']}}</td>
                     </tr>
                  </table>
+
+                 <div class="md:hidden flex flex-row">
+                    <p class="font-medium">
+                   @foreach ($result['subjects'] as $s => $mark)
+                           @if ($subject->find($subject))
+                               <span class="mr-1 text-xs text-black bg-yellow-400 rounded-full px-1 py-1">
+                                   {{title($subject->find($s)->class_subject->level_subject->subject->name)}} - {{$mark}}
+                               </span>
+                           @endif
+                       @endforeach
+
+                       <span class="mr-1 text-xs text-black bg-yellow-300 rounded-full px-1 py-1">
+                           Total - {{$result['total']}}
+                       </span>
+
+                       <span class="mr-1 text-xs text-black bg-yellow-300 rounded-full px-1 py-1">
+                           AVG - {{round(array_sum($result['subjects'])/count($result['subjects']),2)}}
+                       </span>
+
+                       <span class="mr-1 text-xs text-black bg-yellow-300 rounded-full px-1 py-1">
+                           GRD - {{setGrade(array_sum($result['subjects'])/count($result['subjects']))}}
+                       </span>
+
+                       <span class="mr-1 text-xs text-black bg-yellow-300 rounded-full px-1 py-1">
+                           POS - {{$result['position']}}
+                       </span>
+                    </p>
+                </div>
             </div>
         @endif
     @endforeach
